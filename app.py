33aab1b7d8da3e38de5f878f8a5ed7e4157d3562
Freanu Peria <magnussonfrancis@gmail.com>
2021-06-12 20:45:35 +0800

    finally complete.

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 
 from flask import Flask, render_template, request
 
+from logistic_reg import lr_model
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,16 +17,23 @@ def home():
 @app.route('/diagnose', methods=["POST", "GET"])
 def set_values():
     if request.method == "POST":
-        print(request.form['nm'])
-        print(request.form['pregnancy'])
-        print(request.form['glucose'])
-        print(request.form['bp'])
-        print(request.form['skinThickness'])
-        print(request.form['insulin'])
-        print(request.form['bmi'])
-        print(request.form['diabetesPedigree'])
-        print(request.form['age'])
-        return "Hello"
+        pregnancy = float(request.form['pregnancy'])
+        glucose = float(request.form['glucose'])
+        bp = float(request.form['bp'])
+        skin_thickness = float(request.form['skinThickness'])
+        insulin = float(request.form['insulin'])
+        bmi = float(request.form['bmi'])
+        diabetes_pedigree = float(request.form['diabetesPedigree'])
+        age = float(request.form['age'])
+        result = lr_model.predict(
+            pregnancy, glucose, bp, skin_thickness, insulin, bmi,
+            diabetes_pedigree, age
+        )
+        print(result)
+        if result == [1]:
+            return render_template('have-diabetes.html')
+        else:
+            return render_template('no-diabetes.html')
     if request.method == "GET":
         return render_template('setvalues.html')
 
